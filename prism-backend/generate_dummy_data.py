010e17459421e2bd744ecd4e3285e729699938e5
ThrishanthaNS <thrishantha.23cse@cambridge.edu.in>
2026-01-27 19:54:17 +0530

    ha

--- prism-backend/generate_dummy_data.py
+++ prism-backend/generate_dummy_data.py
@@ -13,8 +13,8 @@ import hashlib
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': '2160047',
-    'database': 'prismdbupdated'
+    'password': '@Thrishantha2005',
+    'database': 'prismdatabase'
 }
 
 # Sample data
